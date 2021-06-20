6597ab1c730bcbf6908c8eec395101248d759fb8
yuriy <yuriy@teletronics.ae>
2021-06-20 22:00:10 +0400

    count triplets

--- app/src/main/java/hackerrank/CountTriplets.java
+++ app/src/main/java/hackerrank/CountTriplets.java
@@ -1,26 +1,70 @@
 package hackerrank;
 
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class CountTriplets {
 
     static long countTriplets(List<Long> arr, long r) {
-        //Map<Integer, Integer> countMap = new HashMap<>();
 
-        Map<Long, Long> countMap = arr.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        if (r != 1){
+            return countTripletsBackward(arr, r);
+        }
+        Map<Long, List<Integer>> countMap = new HashMap<>();
+
 
         long count = 0;
-        for (Long k : countMap.keySet()){
-                    if (countMap.containsKey(k * r) && countMap.containsKey(k * r * r)) {
-                        count += countMap.get(k) * countMap.get(k * r) * countMap.get(k * r * r);
+        for (int i=0; i< arr.size(); i++){
+                    if (!countMap.containsKey(arr.get(i))){
+                        countMap.put(arr.get(i), new ArrayList<>());
                     }
-
+                    countMap.get(arr.get(i)).add(i);
        }
+
+        for (int i=0; i< arr.size(); i++){
+            long k = arr.get(i);
+            if (countMap.containsKey(k * r) && countMap.containsKey(k * r * r)) {
+                count += countIndexSecond(i, countMap.get(k*r), countMap.get(k*r*r));
+            }
+        }
+
+        return count;
+    }
+
+    static long countTripletsBackward(List<Long> arr, long r){
+        Map<Long, Integer> values = new HashMap<>();
+        Map<Long, Integer> pairs = new HashMap<>();
+
+
+        long count = 0;
+        for (int i=arr.size()-1; i >= 0; i--){
+            if (pairs.containsKey(arr.get(i)*r)){
+                count += pairs.get(arr.get(i)*r);
+            }
+
+            if (values.containsKey(arr.get(i) * r)){
+                int pv = pairs.getOrDefault(arr.get(i), 0);
+                pairs.put(arr.get(i), pv + values.get(arr.get(i) * r));
+            }
+
+            if (!values.containsKey(arr.get(i))){
+                values.put(arr.get(i), 0);
+            }
+            values.put(arr.get(i), values.get(arr.get(i)) + 1);
+
+        }
+
+        return count;
+    }
+
+    private static long countIndexSecond(int i, List<Integer> l, List<Integer> ls){
+        long count = 0;
+        List<Integer> filtered = l.stream().filter(k -> k>i).collect(Collectors.toList());
+        for (int first : filtered){
+            count += ls.stream().filter(second -> second>first).count();
+        }
         return count;
     }
 }
--- app/src/test/java/hackerrank/CountTripletsTest.java
+++ app/src/test/java/hackerrank/CountTripletsTest.java
@@ -12,10 +12,22 @@ public class CountTripletsTest {
     @Test
     public void countTriplets() {
 
-        Assert.assertEquals(3, CountTriplets.countTriplets(Arrays.asList(1L, 2L, 3L, 1L, 1L, 4L), 2));
+        Assert.assertEquals(1, CountTriplets.countTriplets(Arrays.asList(1L, 2L, 3L, 1L, 1L, 4L), 2));
+
+        Assert.assertEquals(1, CountTriplets.countTriplets(Arrays.asList(1L, 2L, 3L, 1L, 1L, 4L), 2));
         Assert.assertEquals(2, CountTriplets.countTriplets(Arrays.asList(1L, 4L, 16L, 64L), 4));
         Assert.assertEquals(4, CountTriplets.countTriplets(Arrays.asList(1L, 5L, 5L, 25L, 125L), 5));
         Assert.assertEquals(2, CountTriplets.countTriplets(Arrays.asList(1L, 2L, 2L, 4L), 2));
         Assert.assertEquals(6, CountTriplets.countTriplets(Arrays.asList(1L, 3L, 9L, 9L, 27L, 81L), 3));
+        Assert.assertEquals(3, CountTriplets.countTriplets(Arrays.asList(1L, 2L, 1L, 2L, 4L), 2));
+        Assert.assertEquals(0, CountTriplets.countTriplets(Arrays.asList(4L, 2L, 1L), 2));
+        Assert.assertEquals(4, CountTriplets.countTriplets(Arrays.asList(1L, 1L, 1L, 1L), 1));
+        Assert.assertEquals(10, CountTriplets.countTriplets(Arrays.asList(1L, 1L, 1L, 1L, 1L), 1));
+        Assert.assertEquals(20, CountTriplets.countTriplets(Arrays.asList(1L, 1L, 1L, 1L, 1L, 1L), 1));
+        Assert.assertEquals(35, CountTriplets.countTriplets(Arrays.asList(1L, 1L, 1L, 1L, 1L, 1L, 1L), 1));
+        Assert.assertEquals(1, CountTriplets.countTriplets(Arrays.asList(1L, 1L, 1L), 1));
+        Assert.assertEquals(0, CountTriplets.countTriplets(Arrays.asList(1L, 4L, 2L), 2));
+        Assert.assertEquals(1, CountTriplets.countTriplets(Arrays.asList(1L, 4L, 2L, 4L, 2L), 2));
+        Assert.assertEquals(6, CountTriplets.countTriplets(Arrays.asList(1L, 2L, 2L, 4L, 2L, 2L, 4L, 2L), 2));
     }
 }
